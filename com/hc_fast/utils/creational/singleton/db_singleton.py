82408c8e7beff7b5c37d060610092e614427b062
junyeongccom <junyeongc1000@gmail.com>
2025-03-30 18:15:03 +0900

    싱글턴 수정

--- com/hc_fast/utils/creational/singleton/db_singleton.py
+++ com/hc_fast/utils/creational/singleton/db_singleton.py
@@ -16,6 +16,11 @@ class DBConfigSingleton:
         self.port = os.getenv("POSTGRES_PORT", "5432")
         self.database = os.getenv("POSTGRES_DB")
         self.db_url = f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+        print("🔐 POSTGRES_PASSWORD:", os.getenv("POSTGRES_PASSWORD"))
+        print("🔐 POSTGRES_USER:", os.getenv("POSTGRES_USER"))
+        print("🔐 POSTGRES_HOST:", os.getenv("POSTGRES_HOST"))
+        print("🔐 POSTGRES_PORT:", os.getenv("POSTGRES_PORT"))
+        print("🔐 POSTGRES_DB:", os.getenv("POSTGRES_DB"))
 
 # ✅ 전역에서 사용할 인스턴스를 이 안에서 생성해둠
 db_singleton = DBConfigSingleton()
